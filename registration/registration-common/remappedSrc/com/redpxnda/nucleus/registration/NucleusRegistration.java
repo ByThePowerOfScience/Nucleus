8218d52f2e335dbbd2eb11b9b0415aac7e632781
Panda <grames8888@gmail.com>
2024-07-01 18:39:23 -0400

    mojmap translation

---
+++ registration/registration-common/remappedSrc/com/redpxnda/nucleus/registration/NucleusRegistration.java
@@ -0,0 +1,9 @@
+package com.redpxnda.nucleus.registration;
+
+public class NucleusRegistration {
+    public static final String MOD_ID = "nucleus_registration";
+    
+    public static void init() {
+        
+    }
+}
